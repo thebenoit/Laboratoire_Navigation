e143909e540b61525a5c1226cc21be0337aea3ca
thebenoit <kemmywillbenoit@yahoo.com>
2024-06-05 14:31:20 -0400

    ajout de tout les dossiers

--- App.js
+++ App.js
@@ -1,55 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native'
-import Ionicons from '@expo/vector-icons/Ionicons'
-import { AntDesign } from '@expo/vector-icons';
-import { FontAwesome6 } from '@expo/vector-icons';
-import voyages from './trips.json'
-import Accueil from './component/Pages/Accueil'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import React, { useState } from "react";
+import Boulangeries from "./component/BoulangerieListe";
+import boulangeriesData from "./commerces.json";
+import Map from "./component/Carte";
 
-
-const Tab = createBottomTabNavigator();
-
-//const Accueil = ({navigation}) => <View><Text>Accueil</Text></View>
-const Recherche = () => <View><Text>Recherche</Text></View>
-const Croisiere = () => <View><Text>Croisière</Text></View>
 export default function App() {
-
-  const SIZE_ICON = 30;
-  //const COLOR_ICON = focused ? "black" : "blue";
+  const [selectedMarkerId, setSelectedMarkerId] = useState(null)
+  const [activerPressable, setActiverPressable] = useState(false);
+    const rendreVrai = () => {
+      
+      setActiverPressable(true)
+      
+      
+    }
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Accueil">
-        <Tab.Screen name="Accueil" component={Accueil}
-        options={{tabBarIcon: ({size,focused,color}) => 
-          <Ionicons name="home" size={SIZE_ICON} 
-          color={focused ? "blue" : "#ADD8E6"}/>}}
-        />
-        <Tab.Screen name="Recherche" component={Recherche}
-        options={{tabBarIcon: ({size,focused,color}) => 
-        <AntDesign name="search1" size={SIZE_ICON} 
-        color={focused ? "blue" : "#ADD8E6"}/>}}
-        />
-        <Tab.Screen name="Croisière" component={Croisiere}
-        options={{tabBarIcon: ({size,focused,color}) => 
-        <FontAwesome6 name="sailboat" size={SIZE_ICON} 
-         color={focused ? "blue" : "#ADD8E6"}/>}}
-        />
-
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text style={styles.HeaderStyle}>Les Boulangeries</Text>
+      <Boulangeries 
+        data={boulangeriesData} 
+        setActiver={rendreVrai}
+        activer={activerPressable} 
+        selectedMarker={selectedMarkerId}
+        setSelectedMarker={setSelectedMarkerId}
+      />
+      <Map 
+        boulangeries={boulangeriesData} 
+        setActiver={rendreVrai}
+        activer={activerPressable}
+        selectedMarker={selectedMarkerId}
+        setSelectedMarker={setSelectedMarkerId}
+      />
+    </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  HeaderStyle: {
+    fontSize: 25,
+    padding: 10,
+    margin: 15,
+    marginTop: 45,
+    fontWeight: "bold",
   },
 });
